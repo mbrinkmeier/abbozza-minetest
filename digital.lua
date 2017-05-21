d969fc4a0d8214c749aacddac03302c4fea371c7
Michael Brinkmeier <michael.brinkmeier@uni-osnabrueck.de>
2017-05-21 13:26:26 +0200

    license added

--- digital.lua
+++ digital.lua
@@ -1,3 +1,25 @@
+--
+-- abbozza! minetest plugin
+--
+-- Copyright 2017 Michael Brinkmeier ( michael.brinkmeier@uni-osnabrueck.de )
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--   http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+--
+--
+-- The digital I/O-blocks for the abbozza! minetest mod
+--
+
 digital_punched = function(pos, node, puncher, pointed_thing)
   	local meta = minetest.get_meta(pos)
   	meta:set_string("formspec",
@@ -28,8 +50,8 @@ minetest.register_node("abbozza:digital_off", {
                local meta = minetest.get_meta(pos)
                local pin = meta:get_string("pin")
                if pin ~= "" then
-                  abz_send(pos,"dset " .. pin .. " 1")
-   		         minetest.swap_node(pos, {name = "abbozza:digital_on", param2 = node.param2} )
+                  abz_send(pos,"dset " .. pin .. " 1",0)
+   	          minetest.swap_node(pos, {name = "abbozza:digital_on", param2 = node.param2} )
                end
             end
 	 }},
@@ -39,15 +61,6 @@ minetest.register_node("abbozza:digital_off", {
     after_place_node = function(pos, node, puncher, pointed_thing)
     	digital_punched(pos, node, puncher, pointed_thing)
 	 end
-	 --[[ ,
- 	 drawtype = "nodebox",
-	 node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.5, -0.5, -0.5,	0.5, -0.25, 0.5},
-			{-0.1, -0.25, -0.1,  0.1, 0.5, 0.1},
-		},
-	} ]]       
 })
 
 
@@ -61,8 +74,8 @@ minetest.register_node("abbozza:digital_on", {
                local meta = minetest.get_meta(pos)
                local pin = meta:get_string("pin")
                if pin ~= "" then
-                  abz_send(pos,"dset " .. pin .. " 0",pos)
-		            minetest.swap_node(pos, {name = "abbozza:digital_off", param2 = node.param2})
+                  abz_send(pos,"dset " .. pin .. " 0",0)
+		  minetest.swap_node(pos, {name = "abbozza:digital_off", param2 = node.param2})
 		         end
             end
 	}},
@@ -72,15 +85,6 @@ minetest.register_node("abbozza:digital_on", {
  	on_receive_fields = function(pos, formname, fields, player)
  	 	digital_receive_fields(pos, formname, fields, player)
    end
-    --[[ ,
- 	 drawtype = "nodebox",
-	 node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.5, -0.5, -0.5,	0.5, -0.25, 0.5},
-			{-0.1, -0.25, -0.1,  0.1, 0.5, 0.1},
-		},
-	} ]]
 })
 
 
@@ -136,20 +140,18 @@ minetest.register_node("abbozza:digital_in_inactive", {
 minetest.register_abm({
 	nodenames = {"abbozza:digital_in_off" , "abbozza:digital_in_on", "abbozza:digital_in_inactive"},
 	interval = 2.0, -- Run every 1 second
-	chance = 1, -- Select every 1 in 50 nodes
+	chance = 1, -- run each time
 	action = function(pos, node, active_object_count, active_object_count_wider)
 	   local meta = minetest.get_meta(pos)
 	   local pin = meta:get_string("pin")
-	   abz_send(pos,"dget " .. pin)
+	   abz_send(pos,"dget " .. pin, abz_timeout)
 	   
 	   -- check the message
 		local response = meta:get_string("msg")
-		-- print("received: " .. response)
 		meta:set_string("msg",nil)
 		
 	   if response ~= nil and response ~= "" then
 	      local state = string.match(response,".*DVAL.*(%d+)")
-	      -- print("state: " .. state)
 		   if state == "1" then
 		      minetest.swap_node(pos, {name = "abbozza:digital_in_on", param2 = node.param2})
 				mesecon.receptor_on(pos, nil)
